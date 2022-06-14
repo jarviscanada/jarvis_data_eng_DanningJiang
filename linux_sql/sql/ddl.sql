8306e49fe1c0408917ce87a683fcc9c91d19ea56
dn717 <60439063+dn717@users.noreply.github.com>
2022-06-14 17:07:33 -0400

    Update ddl.sql

--- linux_sql/sql/ddl.sql
+++ linux_sql/sql/ddl.sql
@@ -2,16 +2,23 @@
 CREATE TABLE PUBLIC.host_info 
   ( 
      id               SERIAL NOT NULL PRIMARY KEY, 
-     hostname         VARCHAR NOT NULL UNIQUE, 
-     cpu_number       INTEGER NOT NULL,
-     cpu_architecture VARCHAR NOT NULL,
-     cpu_model        VARCHAR NOT NULL,
-     cpu_mhz          float8 NOT NULL,
+     hostname         TEXT NOT NULL UNIQUE, 
+	   cpu_number       INTEGER NOT NULL,
+     cpu_architecture VARCHAR(10) NOT NULL,
+     cpu_model        TEXT NOT NULL,
+     cpu_mhz          REAL NOT NULL,
      L2_cache         INTEGER NOT NULL,
      total_mem        INTEGER NOT NULL,
-     timestamp        TIMESTAMP NOT NULL
+     "timestamp"      TIMESTAMP NOT NULL
   );
+  
+  CREATE TABLE PUBLIC.host_usage 
+  ( 
+     "timestamp"    TIMESTAMP NOT NULL, 
+     host_id        SERIAL NOT NULL references host_info(id),
+	   memory_free    INTEGER NOT NULL,
+     cpu_idle       INTEGER NOT NULL,
+     disk_io        INTEGER NOT NULL,
+     disk_available INTEGER NOT NULL
+  ); 
 
--- DML
--- INSERT statement
-INSERT INTO host_info (id, hostname ...
