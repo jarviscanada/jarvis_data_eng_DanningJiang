26ac08cd1a9a4b71d4825c0ad7c820add3c1d0e0
dn717 <60439063+dn717@users.noreply.github.com>
2022-06-13 23:21:25 -0400

    Update host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -13,6 +13,6 @@ cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"|egrep "^Architecture:"|awk '{print $2}'|xargs) #x86_64
 cpu_model=Intel(R) Xeon(R) CPU @ 2.30GHz
 cpu_mhz=2299.998
-l2_cache=256K
+l2_cache=256 #in KB
 total_mem=$(grep MemTotal /proc/meminfo|awk '{print $2}') #7489612 #in KB
 timestamp=$(date "+%Y-%m-%d %H:%M:%S") #current timestamp in `2019-11-26 14:40:19` format 
