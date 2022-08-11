4a13f1a4dbe9ec0f8f59d4764155bd0e4a2ccd55
dn717 <djiang3@lakeheadu.ca>
2022-08-11 03:47:59 +0000

    first coding challenge

---
+++ practice/src/test/java/ca/jrvs/practice/codingChallenge/OddEvenTest.java
@@ -0,0 +1,14 @@
+package ca.jrvs.practice.codingChallenge;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class OddEvenTest {
+
+    @Test
+    public void oddEvenMod() {
+        OddEven obj = new OddEven();
+        System.out.println(obj.oddEvenMod(1));
+    }
+}
